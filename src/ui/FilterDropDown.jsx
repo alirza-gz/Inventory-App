18434423953a45fee4d6c78838cfa6db32db59bd
Alireza Gholizadeh <alireza.niceee@gmail.com>
2024-08-21 22:46:50 +0330

    Update FilterDropDown.jsx

--- src/ui/FilterDropDown.jsx
+++ src/ui/FilterDropDown.jsx
@@ -1,14 +1,28 @@
 import { useSearchParams } from "react-router-dom";
 import Select from "./Select";
+import { useEffect } from "react";
 
-function FilterDropDown({ options, filterField }) {
+function FilterDropDown({ options, filterField, defaultValue = "" }) {
   const [searchParams, setSearchParams] = useSearchParams();
 
-  const value = searchParams.get(filterField);
+  useEffect(() => {
+    let value = searchParams.get(filterField);
+
+    if (!value && options.length > 0) {
+      value = defaultValue || options[0].value;
+      searchParams.set(filterField, value);
+      setSearchParams(searchParams);
+    }
+  }, [searchParams, setSearchParams, filterField, defaultValue, options]);
+
+  const value = searchParams.get(filterField) || defaultValue;
+
   const handleChange = (e) => {
-    searchParams.set(filterField, e.target.value);
+    const value = e.target.value;
+    searchParams.set(filterField, value);
     setSearchParams(searchParams);
   };
+
   return (
     <div className="w-full">
       <Select value={value} onChange={handleChange} options={options} />
